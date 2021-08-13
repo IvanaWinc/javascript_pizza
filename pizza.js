2a8e4da1df3bbf581f776e4d084d2cecb82412a8
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-08-13 13:23:52 +0200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+    console.log("prepare the dough")
+    console.log("but some jummy stuff on top")
+    console.log("slide the pizza in the ove")
+
+}
+
+makePizza();
+makePizza();
+
+
+const makeSushi = function () {
+    console.log("clean the rise")
+    console.log("slice the fish")
+    console.log("roll the sushi")
+
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
